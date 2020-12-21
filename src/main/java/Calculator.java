fa534a7217a1ed9d3fa2e0878f3e661294df618e
otmattison <otmattison@gmail.com>
2020-12-21 21:33:03 +0000

    initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double numberA,numberB;
+		Operations operate = new Operations();
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter your first number");
+		numberA = input.nextDouble();
+		System.out.println("Enter your second number");
+		numberB = input.nextDouble();
+		
+		System.out.println("Select operation");
+		System.out.println("1. addition" +  "\n" + "2. subtraction" + "\n" + "3. multiplication" + "\n" +"4. division");
+		
+		int choice = input.nextInt();
+		switch(choice) {
+		case 1:
+			operate.addition(numberA,numberB);
+			System.out.println("The awnser is " + operate.sum);
+			break;
+		case 2:
+			operate.subtraction(numberA,numberB);
+			System.out.println("The awnser is " + operate.sum);
+
+			break;
+		case 3:
+			operate.multiplication(numberA,numberB);
+			System.out.println("The awnser is " + operate.sum);
+
+			break;
+		case 4:
+			operate.division(numberA,numberB);
+			System.out.println("The awnser is " + operate.sum);
+
+			break;
+		
+		
+	}
+
+}}
---
+++ src/main/java/Operations.java
@@ -0,0 +1,23 @@
+
+public class Operations {
+	double sum;
+	
+	public void addition(double numberA, double numberB) {
+		sum = numberA + numberB;
+		
+	}
+	
+	public void subtraction(double numberA, double numberB) {
+		sum = numberA - numberB;
+	}
+	
+	public void multiplication(double numberA, double numberB) {
+		sum = numberA * numberB;
+	}
+	
+	public void division(double numberA, double numberB) {
+		sum = numberA / numberB;
+	}
+	
+	
+}
